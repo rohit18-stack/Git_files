92669385f0d75d1119f5df09c3b13b28411fa9d7
rohit.shah <rohit.shah@siemens.com>
2024-09-11 15:06:06 +0530

    first push

---
+++ App4.java
@@ -0,0 +1,15 @@
+package linked_hashMap;
+
+import java.util.List;
+
+
+public class App4 {
+    public static void main(String[] args) {   // ~ psvm**
+        // Sorting
+        // using comparators....(collection.sort() method )
+        // using comparable....(interfaces --> class)
+
+        List<String> animals = 
+
+    }
+}
